f4f9949ac3291dcfd15115da48df8eac066b37e6
GabriielND <gabriel_deodato@hotmail.com>
2026-03-19 23:36:33 -0300

    Review Lola Young

--- js/posts.js
+++ js/posts.js
@@ -26,7 +26,7 @@ export const blogPosts = [
     //     'imagem': 'https://i.scdn.co/image/ab67616d0000b27323d41bf736920a032e222a78',
     // },
     {
-        'id': 3,
+        'id': 1,
         'title': 'Escola de Contos Eróticos Para Viúvas',
         'author': 'Gabe',
         'date': '06-01-2026',
@@ -55,7 +55,7 @@ export const blogPosts = [
     //     'imagem': 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1r7p.webp',
     // },
     {
-        'id': 5,
+        'id': 2,
         'title': 'Pillion',
         'author': 'Gabe',
         'date': '13-01-2026',
@@ -68,6 +68,60 @@ export const blogPosts = [
         'tags': ['Filme'],
         'slug': 'pillion',
         'imagem': 'https://image.tmdb.org/t/p/original/7Pd6ChSQjSXy4snJiorSdzg2cG3.jpg',
+    },
+    {
+        'id': 3,
+        'title': 'I\'m Only F**king Myself',
+        'author': 'Gabe',
+        'date': '19-03-2026',
+        'nota': '9',
+        'body': 'Amei o álbum dessa princesa! Uma mistura meio Amy Winehouse e Adele (ou seja: britânica). Ela é meio piradinha, as vezes meio sem pé nem cabeça, mas comigo funcionou demais. Sinto que ela cria sem muita direção, só vai indo até chegar sei lá onde, realmente a messy girl.<br>\
+                 Pelos "hits" dela, eu esperava uma coisa bem animadinha, bem boba, mas ela tem profundidade nas letras, SPIDERS é algo que eu nunca imaginaria saindo dela e depois de ouvir, acho que só poderia ter saído dela mesmo.<br>\
+                 Ninguém faz Lola Young como a Lola Young, e é por isso que eu quero ver essa princesa hitando muito nos próximos anos.<br>\
+                 <br>\
+                 <ul class="favs"><div><em class="musicStar">t</em><p>Favoritas</p></div>\
+                 <li>Not Like That Anymore</li>\
+                 <li>F**K EVERYONE</li>\
+                 <li>SPIDERS</li>\
+                 <li>d£aler</li>\
+                 <li>SAD SOB STORY! :)</li>\
+                 <li>Penny Out of Nothing</li>\
+                 </ul>\
+                 <ul class="favs" id="mencoes">\
+                 <div><em class="musicStar">p</em><p>Menções Honrosas</p></div>\
+                 <li>One Thing</li>\
+                 <li>Walk All Over You</li>\
+                 <li>Post Sex Clarity</li>\
+                 <li>CAN WE IGNORE IT? :(</li>\
+                 </ul>\
+                 <div class="track">\
+                 <h2>F**K EVERYONE</h2>\
+                 <p>DIVA BISSEXUAL!!! Adoro a batidinha dessa, e gosto muito da voz que ela faz, ótimo começo de álbum! (não conto os interlúdios... pq eles são ruins...)</p></div>\
+                 <div class="track">\
+                 <h2>SPIDERS</h2>\
+                 <p>Fiquei em choque quando ouvi essa, foi como achar ouro!<br>\
+                 Essa devia estar tocando em tudo que é lugar, mostra a força que a Lola tem como artista<br>\
+                 A voz dela, a letra, o instrumental, tudo incrível</p></div>\
+                 <div class="track">\
+                 <h2>Not Like That Anymore</h2>\
+                 <p>Eu estou me fudendo, mas também não é bem assim!<br>\
+                 AMO essa palhaçadinha da letra, acho que tudo nela encaixa muito bem, principalmente com o álbum.<br>\
+                 É o famoso "podia estar pior", mas dito (ou melhor, cantado) com alegria, um alívio, não está TUDO bem ainda, mas eu estou chegando lá!</p></div>\
+                 <div class="track">\
+                 <h2>One Thing</h2>\
+                 <p>Oh, hi! Não tenho muito oq falar dessa.<br>\
+                 Otimo single, mereceu a hitada de leve</p></div>\
+                 <div class="track">\
+                 <h2>d£aler</h2>\
+                 <p>MERECIA ser hit, tinha a benção de Elton John pra pegar #1 mas infelizmente não conseguiu coitada.<br>\
+                 Não acho que é a melhor do álbum, mas acho q assim como One Thing é um bom single e um ótimo ponto de entrada pro álbum, entrega só um pouco pra vc ir descobrir as verdadeiras joias do álbum</p></div>\
+                 <div class="track">\
+                 <h2>SAD SOB STORY! :)</h2>\
+                 <p>Essa carinha feliz é um detalhe tão legal, pq o título induz a uma música triste, mas a carinha é a dica de que, na verdade, é sobre algo feliz!<br>\
+                 E até o instrumental vai nessa, brinca nessa linha de ir pra um lado mais triste e um lado mais feliz, o que combina total com a letra</p></div>',
+        'tags': ['Música'],
+        'slug': 'im-only-fucking-myself',
+        'imagem': 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fuproxx.com%2Fwp-content%2Fuploads%2F2025%2F06%2Flola-young-im-only-fcking-myself.jpg%3Fresize%3D300&f=1&nofb=1&ipt=1f534b1457937d86de153973363bb37264118d3156e6d6eff91526a2e7ea3d0b',
     },
 ];
 
